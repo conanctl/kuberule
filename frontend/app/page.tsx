0f35458849ffb55e5505c33f558e6c32687c432e
conanctl <conan@flynn.sh>
2025-12-06 20:59:35 +0000

    feat: initial layout and public assets

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1>Kuberule Dashboard</h1>
+      <p>Kubernetes Security & Compliance Platform</p>
+    </div>
+  );
+}
